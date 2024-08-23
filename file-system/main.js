dd956bc5e0ac36ad72f4960cb331012e026e9ff9
Jalever <jaleverchen@gmail.com>
2024-08-23 14:43:25 +0800

    feat(*): add linter and editor config

--- file-system/main.js
+++ file-system/main.js
@@ -1,14 +1,14 @@
-const {unlink} = require("fs/promises");
-const { join } = require("path");
+const { unlink } = require('fs/promises')
+const { join } = require('path')
 
-async function deleteFile(traget = "") {
-  const filename = join(__dirname, traget);
+async function deleteFile(traget = '') {
+  const filename = join(__dirname, traget)
   try {
-    await unlink(filename);
-    console.log(`deleted ${filename}`);
+    await unlink(filename)
+    console.log(`deleted ${filename}`)
   } catch (error) {
-    console.error(`got an error trying to delete the file: ${error.message}`);
+    console.error(`got an error trying to delete the file: ${error.message}`)
   }
 }
 
-deleteFile(`/sample.txt`);
+deleteFile(`/sample.txt`)
