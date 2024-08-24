d71b0a9266ddbcb0165494ab0d923ec371c51b0c
Jalever <jaleverchen@gmail.com>
2024-08-24 21:21:11 +0800

    feat(http): A Detailed look into the Node.js HTTP module
    feat(http): extract data from the URL

--- src/http/main.js
+++ src/http/main.js
@@ -1,5 +1,8 @@
 const http = require('http')
 const server = http.createServer(function (req, res) {
+  console.log(`Request Headers: >> `, req.headers)
+  console.log(`Request Method: >> `, req.method)
+  console.log(`Request URL: >> `, req.url)
   res.end('Hello World!')
 })
 server.listen(3000, function () {
