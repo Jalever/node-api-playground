6058f9ba12969238cb16179a519d3631d149b5f1
Jalever <jaleverchen@gmail.com>
2024-08-24 21:38:59 +0800

    feat(http): A Detailed look into the Node.js HTTP module
    feat(http): extract request body through stream

--- src/http/main.js
+++ src/http/main.js
@@ -2,10 +2,20 @@ const http = require('http')
 const url = require('url')
 
 const server = http.createServer(function (req, res) {
-  const urlData = url.parse(req.url, true)
-  console.log(`url data: >> `, urlData)
-  res.end('Hello World!')
+  const bodyStream = []
+  req
+    .on('data', function (chunk) {
+      bodyStream.push(chunk)
+    })
+    .on('end', function () {
+      const bufferData = Buffer.concat(bodyStream)
+      const requestBody = JSON.parse(bufferData)
+      console.log('request body :>>', requestBody)
+      res.end('end')
+    })
+
+  console.log('hello world!')
 })
 server.listen(3000, function () {
-  console.log(`Server started on localhost:3000`)
+  console.log('Server started on localhost:3000')
 })
