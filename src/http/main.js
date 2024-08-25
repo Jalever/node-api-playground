edc87494896e75dcda89f65b4bd15e60ca14bf60
Jalever <jaleverchen@gmail.com>
2024-08-25 16:49:48 +0800

    feat(http): A Detailed look into the Node.js HTTP module
    feat(http): router

--- src/http/main.js
+++ src/http/main.js
@@ -2,19 +2,37 @@ const http = require('http')
 const url = require('url')
 
 const server = http.createServer(function (req, res) {
+  res.setHeader('Content-Type', 'application/json')
   const bodyStream = []
-  req
-    .on('data', function (chunk) {
-      bodyStream.push(chunk)
-    })
-    .on('end', function () {
-      const bufferData = Buffer.concat(bodyStream)
-      const requestBody = JSON.parse(bufferData)
-      console.log('request body :>>', requestBody)
-      res.end('end')
-    })
 
-  console.log('hello world!')
+  if (req.url === '/') {
+    const responseData = JSON.stringify({ greeting: 'Hello World!' })
+    res.write(responseData)
+    res.end()
+  } else if (req.url === '/profile') {
+    const responseData = JSON.stringify({ data: 'Profile Page!' })
+    res.write(responseData)
+    res.end()
+  } else if (
+    req.url === '/create' &&
+    req.method === 'POST' &&
+    req.headers['content-type'] === 'application/json'
+  ) {
+    req
+      .on('data', function (chunk) {
+        bodyStream.push(chunk)
+      })
+      .on('end', function () {
+        const bufferData = Buffer.concat(bodyStream)
+        const requestBody = JSON.parse(bufferData)
+        console.log('request body :>>', requestBody)
+        res.end('end')
+      })
+  } else {
+    res.statusCode = 404
+    res.write('Page not found!')
+    res.end()
+  }
 })
 server.listen(3000, function () {
   console.log('Server started on localhost:3000')
