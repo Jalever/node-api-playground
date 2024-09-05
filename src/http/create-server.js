b938c8b4d9f498ffe4e276561df8c515e17ec83d
Jalever <jaleverchen@gmail.com>
2024-09-05 14:07:25 +0800

    feat(stream): 理解Node中的Buffer与stream | 创作者训练营第二期
    feat(stream): stream - Duplex

--- src/http/create-server.js
+++ src/http/create-server.js
@@ -1,15 +1,21 @@
 const http = require('node:http')
 
+let s = ''
+for (let i = 0; i < 1024 * 10; i++) {
+  s += 'a'
+}
+
+const str = s
+const bufferStr = Buffer.from(s)
+
 const server = http.createServer(function (req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-  })
-  res.end(
-    JSON.stringify({
-      data: 'Hello World!',
-    }),
-  )
+  console.log(`req.url: `, req.url)
+  if (req.url === '/buffer') {
+    res.end(bufferStr)
+  } else if (req.url === '/string') {
+    res.end(str)
+  }
 })
 server.listen(3000, function () {
-  console.log('server is listening on port 3000')
+  console.log(`the server is listening on port: 3000`)
 })
---
+++ src/stream/main.js
@@ -0,0 +1,21 @@
+const Duplex = require('stream').Duplex
+const duplex = Duplex()
+
+duplex._read = function () {
+  this._readNum = this._readNum || 0
+  if (this._readNum > 1) {
+    this.push(null)
+  } else {
+    this.push('' + this._readNum++)
+  }
+}
+duplex._write = function (buf, enc, next) {
+  process.stdout.write('_write ' + buf.toString() + '\n')
+  next()
+}
+duplex.on('data', function (data) {
+  console.log('ondata: ', data.toString())
+})
+duplex.write('a')
+duplex.write('b')
+duplex.end()
