f8486bbfa9e510178e74468862dae93390db8a8f
Jalever <jaleverchen@gmail.com>
2024-09-05 14:28:29 +0800

    feat(stream): 理解Node中的Buffer与stream | 创作者训练营第二期
    feat(stream): stream - 使用 pipe + transform 实现进度打印

--- src/stream/main.js
+++ src/stream/main.js
@@ -1,8 +1,21 @@
 const fs = require('fs')
 const zlib = require('zlib')
 const path = require('path')
+const { Transform } = require('stream')
 
-const filePath = path.join(__dirname, '/article.xmind')
-fs.createReadStream(filePath)
+const fileName = path.join(__dirname, '/article.xmind')
+
+const reportProgress = new Transform({
+  transform(chunk, encoding, callback) {
+    process.stdout.write('.')
+    callback(null, chunk)
+  },
+})
+
+fs.createReadStream(fileName)
   .pipe(zlib.createGzip())
-  .pipe(fs.createWriteStream(filePath + '.gz'))
+  .pipe(reportProgress)
+  .pipe(fs.createWriteStream(fileName + '.zz'))
+  .on('finish', function () {
+    console.log('Done')
+  })
