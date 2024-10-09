55026e73b532285a1fe61aceafc42f6c2f30affc
Jalever <jaleverchen@gmail.com>
2024-10-09 14:58:17 +0800

    feat: add faker

--- src/stream/main.js
+++ src/stream/main.js
@@ -1,19 +1,46 @@
-const { pipeline } = require('stream')
-const fs = require('fs')
-const zlib = require('zlib')
-const path = require('path')
+// const { truncateSync } = require('node:fs')
 
-const fileName = path.join(__dirname, '/article')
+// const filePath = __dirname + '/sample.txt'
+// try {
+//   truncateSync(filePath)
+// } catch (error) {
+//   throw new Error(error)
+// }
 
-pipeline(
-  fs.createReadStream(fileName + '.xmind'),
-  zlib.createGzip(),
-  fs.createWriteStream(fileName + '.tar.gz'),
-  function (err) {
-    if (err) {
-      console.log('failure', err)
-    } else {
-      console.log('success')
-    }
-  },
+const { appendFileSync, statSync } = require('node:fs')
+const { Faker, zh_CN } = require('@faker-js/faker')
+
+const customFaker = new Faker({
+  locale: [zh_CN],
+})
+
+const filePath = __dirname + '/sample.txt'
+const bytesByMb = 1024 ** 2
+const bytesByKb = 1024
+// const writeStream = createWriteStream(filePath)
+
+// let fragments = ''
+const count = 20_000
+
+for (let i = count; i > 0; i--) {
+  try {
+    const content = customFaker.person.fullName()
+    const newContent = `[${i}]${content}`
+    appendFileSync(filePath, newContent)
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const stats = statSync(filePath)
+console.log(
+  `Write Successfully, size(kb): ${stats.size / bytesByKb}, size(mb): ${
+    stats.size / bytesByMb
+  }`,
 )
+
+// writeStream.write(fragments)
+// writeStream.end()
+// writeStream.on('finish', function () {
+//   console.log(`Write Successfully`)
+// })
